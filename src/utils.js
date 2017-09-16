cddef1f9586395b2bd540e6defdf316b37152d24
Nicolás Alliaume <nicolas@on-lab.com>
2017-09-16 02:53:03 -0400

    tasks description trimmed for the list

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,14 @@ String.prototype.capitalizeFirst = function() {
 	return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
+String.prototype.trimToWords = function(maxCharLength) {
+	if (this.length <= maxCharLength) return this;
+	var trimmedString = this.substr(0, maxCharLength);
+	trimmedString = trimmedString.substr(0, 
+		Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+	return trimmedString + '...';
+}
+
 Array.prototype.empty = function() {
 	return this.length === 0;
 }
