c6d4d5765b58f8955dd47d7f6cbf4677d1ae7b48
Nicolás Alliaume <nicolas@on-lab.com>
2017-09-22 17:36:09 -0400

    error handling + info messages rico rico.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Container, Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
+import MessagesBar from '../components/messages/MessagesBar';
 import Icon from '../components/misc/Icon';
 
 import Dashboard from './Dashboard';
@@ -26,6 +27,7 @@ export default class App extends Component {
 					</Nav>
 				</Navbar>
 				<Container fluid id='main'>
+					<MessagesBar />
 					<Switch>
 						<Route exact path='/' component={Dashboard} />
 						<Route path='/tasks' component={Tasks} />
