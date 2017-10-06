fa1e6be01a3f4740001ce7ac4e7bb955473d8aa3
Nicolás Alliaume <nicolas@on-lab.com>
2017-10-06 17:49:04 -0400

    Building and pushing to heroku

--- src/components/activity/ActivityListItem.js
+++ src/components/activity/ActivityListItem.js
@@ -5,8 +5,7 @@ import moment from 'moment';
 export default class ActivityListItem extends Component {
 	render() {
 		const { activity } = this.props;
-		// const profileImage = activity.user.profile_image;
-		const profileImage = 'http://lorempixel.com/200/200/cats/';
+		const profileImage = activity.user.profile_image;
 		return (
 			<li className='activity-list-item'>
 				<Card className='latest-activity'>
