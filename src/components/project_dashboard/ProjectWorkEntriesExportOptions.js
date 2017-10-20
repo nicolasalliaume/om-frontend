db04f37e7484bd3a45ed1ee42bd4ca3175b9d0bc
Nicolás Alliaume <nicolas@on-lab.com>
2017-10-20 16:45:21 -0400

    style fix for work entries export on project dashboard

--- src/components/project_dashboard/ProjectWorkEntriesExportOptions.js
+++ src/components/project_dashboard/ProjectWorkEntriesExportOptions.js
@@ -11,7 +11,7 @@ import Icon from '../misc/Icon';
 export default class ProjectWorkEntriesExportOptions extends Component {
 	render() {
 		return (
-			<Row>
+			<Row className='project-work-entries-options'>
 				<Col xs={12} className='text-right'>
 					<ExternalUrllLink url={this.props.htmlLink}>
 						<Icon fa-share-square-o />
