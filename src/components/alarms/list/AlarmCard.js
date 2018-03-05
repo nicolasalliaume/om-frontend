afea0098e6be01de08c54fc4d50a8d6b62af0ab0
Nicolás Alliaume <nicolas@on-lab.com>
2018-03-05 10:15:35 -0500

    ++ duplicate alarm ++ mute/unmute alarm

--- src/components/alarms/list/AlarmCard.js
+++ src/components/alarms/list/AlarmCard.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { Col, Card, CardBlock, CardTitle, Button } from 'reactstrap';
 import EditAlarmModalForm from '../forms/EditAlarmModalForm';
+import update from 'immutability-helper';
+import { connect } from 'react-redux';
+import { updateAlarm, createAlarm } from '../../../actions/alarms';
 
-export default class AlarmCard extends Component {
+class AlarmCard extends Component {
 	constructor() {
 		super();
 		this.state = { modal : false };
 	}
+
 	toggleModal = () => this.toggleState('modal');
 	toggleState = (name) => this.setState({ [name] : !this.state[name] })
+
 	render() {
 		const { alarm } = this.props;
 		const stateClass = alarm.enabled ? 'enabled' : 'disabled';
@@ -17,6 +22,14 @@ export default class AlarmCard extends Component {
 				<Card className={`alarm-card ${stateClass}`}>
 					<CardBlock className='card-body'>
 						<CardTitle>{alarm.name}</CardTitle>
+						<div className='quick-options'>
+							<Button onClick={this.toggleEnabled}>
+								<i className={`fa fa-bell${alarm.enabled ? '-slash' : ''}`} />
+							</Button>
+							<Button onClick={this.duplicate}>
+								<i className='fa fa-copy' />
+							</Button>
+						</div>
 						<div className='author'>{alarm.created_by.full_name}</div>
 						<div className='text-center'>
 							<Button className='view-details' onClick={this.toggleModal}>
@@ -32,4 +45,23 @@ export default class AlarmCard extends Component {
 			</Col>
 		)
 	}
+
+	toggleEnabled = () => {
+		const { alarm } = this.props;
+		this.props.updateAlarm(alarm._id, update(alarm, {
+			enabled: {$set: !alarm.enabled}
+		}))
+	}
+
+	duplicate = () => {
+		const newAlarm = update(this.props.alarm, { $unset: ['_id'] });
+		this.props.createAlarm(newAlarm);
+	}
 }
+
+const mapDispatchToProps = dispatch => ({
+	updateAlarm : (alarmId, update) => dispatch(updateAlarm(alarmId, update)),
+	createAlarm : (alarmId, update) => dispatch(createAlarm(alarmId, update)),
+})
+
+export default connect(null, mapDispatchToProps)(AlarmCard);
