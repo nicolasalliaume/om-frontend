5e86d35057043a4214ed9417c8314da3abaadf67
Nicolás Alliaume <nicolas@on-lab.com>
2018-06-22 18:45:46 -0400

    fixed main nav error about key missing

--- src/components/misc/LinkWithTooltip.js
+++ src/components/misc/LinkWithTooltip.js
@@ -3,12 +3,14 @@ import { UncontrolledTooltip } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 export default function LinkWithTooltip(props) {
-	return [
-		<Link to={props.to} id={props.id}>
-			{props.children}
-		</Link>,
-		<UncontrolledTooltip placement="right" target={props.id} delay={{show: 3, hide: 0}}>
-			{props.tooltip}
-		</UncontrolledTooltip>
-	]
+	return (
+		<React.Fragment>
+			<Link to={props.to} id={props.id}>
+				{props.children}
+			</Link>
+			<UncontrolledTooltip placement="right" target={props.id} delay={{show: 3, hide: 0}}>
+				{props.tooltip}
+			</UncontrolledTooltip>
+		</React.Fragment>
+	)
 }
