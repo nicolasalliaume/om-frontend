205d1b056a71836d13546302195954d96653fa8f
Nicolás Alliaume <nicolas@on-lab.com>
2018-08-10 12:07:40 -0400

    -- dotenv req

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import './styles/Layout.css';
 
-require('dotenv').config();
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
