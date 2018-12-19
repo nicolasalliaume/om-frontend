533cea5bdc2b40df41b2aaf0cc91a376aced4b90
Nicolás Alliaume <nicolas@on-lab.com>
2018-12-19 14:21:28 -0300

    ++ billable hours in project dashboard

--- src/components/misc/StatusBars.js
+++ src/components/misc/StatusBars.js
@@ -4,13 +4,13 @@ import { UncontrolledTooltip } from 'reactstrap';
 export default class StatusBars extends Component {
 	render() {
 		const orderedBars = this.props.config.slice();
-		orderedBars.sort((a, b) => {
-			if (a.start === b.start) {
-				return (a.width < b.width) ? 1 : -1;
+		orderedBars.sort( ( a, b ) => {
+			if ( a.start === b.start ) {
+				return b.width - a.width;
 			}
 			return a.start < b.start ? 1 : -1;
-		})
-		const bars = orderedBars.map((b, idx) => {
+		} );
+		const bars = orderedBars.map( ( b, idx ) => {
 			const uniqueId = this.getId();
 			const style = { 
 				width : b.width + '%', 
@@ -22,17 +22,17 @@ export default class StatusBars extends Component {
 						<div className="content">{b.label}</div>
 					</div>
 					{ b.tooltip && (
-						<UncontrolledTooltip placement="right" target={uniqueId} delay={{show: 1000, hide: 0}}>
+						<UncontrolledTooltip placement="right" target={uniqueId} delay={{ show: 1000, hide: 0 }}>
 							{b.tooltip}
 						</UncontrolledTooltip>
 					)}
 				</React.Fragment>
-			)
-		})
+			);
+		} );
 		return (
 			<div className="status-bars">{bars}</div>
-		)
+		);
 	}
 
-	getId = _ => '_' + Math.random().toString(36).substr(2, 9)
+	getId = _ => '_' + Math.random().toString( 36 ).substr( 2, 9 )
 }
--- src/components/project_dashboard/ProjectBalanceCard.js
+++ src/components/project_dashboard/ProjectBalanceCard.js
@@ -5,24 +5,27 @@ import StatusBars from '../misc/StatusBars';
 export default class ProjectBalanceCard extends Component {
 	render() {
 		const { project } = this.props;
-		if (!project) return <div/>;
+		if ( !project ) return <div/>;
 
 		const income = project.billed_amount_total;
 		const outcome = project.expenses_amount_total;
-		const executed = Math.round(project.executed_hours_total);
-		const max = Math.max(income, outcome);
+		const executed = Math.round( project.executed_hours_total );
+		const max = Math.max( income, outcome );
+		const hoursBilled = this.getHoursBilled();
+		const billableHours = hoursBilled < project.executed_hours_total 
+			? project.executed_hours_total - hoursBilled : 0;
 
 		const bars = [
 			{
 				class : 'green',
 				start : 0,
-				width : income === max ? 100 : (income/max)*100
+				width : ( income / ( income + outcome ) ) * 100,
 			},{
-				class : 'blue',
+				class : 'red',
 				start : 0,
-				width : outcome === max ? 100 : (outcome/max)*100
+				width : 100
 			}
-		]
+		];
 
 		return (
 			<Card className='balance-card project-balance text-center'>
@@ -31,11 +34,11 @@ export default class ProjectBalanceCard extends Component {
 					<Row>
 						<Col xs={6}>
 							<h6>Income</h6>
-							<span className='money income'>{income.toFixed(0)}</span>
+							<span className='money income'>{income.toFixed( 0 )}</span>
 						</Col>
 						<Col xs={6}>
 							<h6>Outcome</h6>
-							<span className='money outcome'>{outcome.toFixed(0)}</span>
+							<span className='money outcome'>{outcome.toFixed( 0 )}</span>
 						</Col>
 					</Row>
 					<Row>
@@ -45,8 +48,18 @@ export default class ProjectBalanceCard extends Component {
 					</Row>
 					<Row>
 						<Col xs={6}>
-							<h6>Hours</h6>
-							<span className='balance-hours income'>{executed}</span>
+							<h6>Executed</h6>
+							<span className='balance-hours executed'>{executed}</span>
+						</Col>
+						<Col xs={6}>
+							<h6>Billed</h6>
+							<span className='balance-hours income'>{hoursBilled}</span>
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={6}>
+							<h6>Billable</h6>
+							<span className='balance-hours billable'>{billableHours}</span>
 						</Col>
 						<Col xs={6}>
 							<h6>$/HR</h6>
@@ -55,6 +68,13 @@ export default class ProjectBalanceCard extends Component {
 					</Row>
 				</CardBody>
 			</Card>
-		)
+		);
+	}
+
+	getHoursBilled() {
+		const p = this.props.project;
+		return p.hours_sold_unit === 'monthly' 
+			? p.billed_hours_month 
+			: p.billed_hours_total;
 	}
 }
