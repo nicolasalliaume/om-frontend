ba5cbd45a7464ec898865aa9766c45f37991cba9
Nicolás Alliaume <nicolas@on-lab.com>
2019-02-08 15:36:36 -0300

    fixed month selector

--- src/components/misc/MonthSelector.js
+++ src/components/misc/MonthSelector.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Input } from 'reactstrap';
 
-function withZero(i) {
-	return String(i+100).substring(1);
+function withZero( i ) {
+	return String( i+100 ).substring( 1 );
 }
 
-export default function MonthSelector(props) {
+export default function MonthSelector( props ) {
 	const options = [];
-	for (var i = 1; i < 12; i++) {
-		options.push({ value: withZero(i), label: withZero(i) })
+	for ( var i = 1; i <= 12; i++ ) {
+		options.push( { value: withZero( i ), label: withZero( i ) } );
 	}
 
 	return (
 		<Input type="select" 
 			className='month-selector'
 			value={props.value} 
-			onChange={e => props.onChange(e.target.value)}>
+			onChange={e => props.onChange( e.target.value )}>
 			
-			{ options.map((i) => 
+			{ options.map( ( i ) => 
 				<option key={i.label} value={i.value}>{i.label}</option>
 			)}
 		</Input>
-	)
+	);
 }
